5cc5446c9beaf5922ef46eeec42399a5061955d5
Samuel <awosamuel@gmail.com>
2022-06-30 08:20:33 -0700

    my_calculator.py

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -3,32 +3,31 @@ from calculator_1 import add, sub, mul, div
 def main():
 
     import sys
-    for i in range(1, len(sys.argv)):
-        if len(sys.argv) > 3:
-            print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-            exit(1)
-        else:
-            if len(sys.argv) == 1:
-                a = sys.argv[i] 
-            elif len(sys.argv) == 3:
-                b = sys.argv[i]
-            elif len(sys.argv) == 2:
-                if sys.argv[i] == '+':
-                    operator = sys.argv[i]
-                    c = add(a, b)
-                elif sys.argv[i] == '-':
-                    operator = sys.argv[i]
-                    c = sub(a, b)
-                elif sys.argv[i] == '*':
-                    operator = sys.argv[i]
-                    c = mul(a, b)
-                elif sys.argv[i] == '/':
-                    operator = sys.argv[i]
-                    c = div(a, b)
-                else:
-                    print("Unknown operator. Available operators: +, -, * and /")
-                    exit(1)
-            print("{} {} {} = {}".format(a, operator, b, c))
+    if len(sys.argv) > 3:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
+    else:
+        if len(sys.argv) == 1:
+            a = int(sys.argv[1]) 
+        elif len(sys.argv) == 3:
+            b = int(sys.argv[3])
+        elif len(sys.argv) == 2:
+            if sys.argv[2] == '+':
+                operator = sys.argv[2]
+                c = add(a, b)
+            elif sys.argv[2] == '-':
+                operator = sys.argv[2]
+                c = sub(a, b)
+            elif sys.argv[2] == '*':
+                operator = sys.argv[2]
+                c = mul(a, b)
+            elif sys.argv[2] == '/':
+                operator = sys.argv[2]
+                c = div(a, b)
+            else:
+                print("Unknown operator. Available operators: +, -, * and /")
+                exit(1)
+        print("{} {} {} = {}".format(a, operator, b, c))
 
 
 if __name__ == "__main__":
