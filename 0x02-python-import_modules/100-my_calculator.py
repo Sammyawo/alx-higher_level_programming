b1677b2b5ea0f2f3620168c13439b6d214be79f4
Samuel <awosamuel@gmail.com>
2022-06-30 08:48:38 -0700

    my_calculator.py

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -3,31 +3,26 @@ from calculator_1 import add, sub, mul, div
 def main():
 
     import sys
-    if len(sys.argv) > 3:
+    if len(sys.argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
     else:
-        if len(sys.argv) == 1:
-            a = int(sys.argv[1]) 
-        elif len(sys.argv) == 3:
-            b = int(sys.argv[3])
-        elif len(sys.argv) == 2:
-            if sys.argv[2] == '+':
-                operator = sys.argv[2]
-                c = add(a, b)
-            elif sys.argv[2] == '-':
-                operator = sys.argv[2]
-                c = sub(a, b)
-            elif sys.argv[2] == '*':
-                operator = sys.argv[2]
-                c = mul(a, b)
-            elif sys.argv[2] == '/':
-                operator = sys.argv[2]
-                c = div(a, b)
-            else:
-                print("Unknown operator. Available operators: +, -, * and /")
-                exit(1)
-        print("{} {} {} = {}".format(a, operator, b, c))
+        a = sys.argv[1]
+        b = sys.argv[3]
+         sign = ['+', '-', '*', '/']
+         for i in sign:
+             if sys.argv[2] == i:
+                 if sys.argv[2] == sign[0]:
+                     print("{:s} + {:s} = {:d}".format(a, b, add(int(a), int(b))))
+                 elif sys.argv[2] == sign[1]:
+                     print("{:s} - {:s} = {:d}".format(a, b, sub(int(a), int(b))))
+                 elif sys.argv[2] == sign[2]:
+                     print("{:s} * {:s} = {:d}".format(a, b, mul(int(a), int(b))))
+                 elif sys.argv[3] == sign[3]:
+                     print("{:s} / {:s} = {:d}".format(a, b, div(int(a), int(b))))
+             else:
+                 print("Unknown operator. Available operators: +, -, * and /")
+                 exit(1)
 
 
 if __name__ == "__main__":
