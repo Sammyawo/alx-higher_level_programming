f407b67efa4db4ad80100d49af2f5b38bbee3899
Samuel <awosamuel@gmail.com>
2022-06-30 08:57:18 -0700

    my_calculator.py

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -9,20 +9,24 @@ def main():
     else:
         a = sys.argv[1]
         b = sys.argv[3]
+        p = 0
          sign = ['+', '-', '*', '/']
          for i in sign:
              if sys.argv[2] == i:
-                 if sys.argv[2] == sign[0]:
-                     print("{:s} + {:s} = {:d}".format(a, b, add(int(a), int(b))))
-                 elif sys.argv[2] == sign[1]:
-                     print("{:s} - {:s} = {:d}".format(a, b, sub(int(a), int(b))))
-                 elif sys.argv[2] == sign[2]:
-                     print("{:s} * {:s} = {:d}".format(a, b, mul(int(a), int(b))))
-                 elif sys.argv[3] == sign[3]:
-                     print("{:s} / {:s} = {:d}".format(a, b, div(int(a), int(b))))
-             else:
-                 print("Unknown operator. Available operators: +, -, * and /")
-                 exit(1)
+                 p = 1
+                 break
+        if p == 1:
+            if sys.argv[2] == sign[0]:
+                print("{:s} + {:s} = {:d}".format(a, b, add(int(a), int(b))))
+            elif sys.argv[2] == sign[1]:
+                print("{:s} - {:s} = {:d}".format(a, b, sub(int(a), int(b))))
+            elif sys.argv[2] == sign[2]:
+                print("{:s} * {:s} = {:d}".format(a, b, mul(int(a), int(b))))
+            elif sys.argv[3] == sign[3]:
+                print("{:s} / {:s} = {:d}".format(a, b, div(int(a), int(b))))
+        else:
+            print("Unknown operator. Available operators: +, -, * and /")
+            exit(1)
 
 
 if __name__ == "__main__":
