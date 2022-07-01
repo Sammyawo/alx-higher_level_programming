abc5c7b5d09b161ee0f78f7352dbaf42c37e1e95
Samuel <awosamuel@gmail.com>
2022-07-01 04:19:16 -0700

    hidden_discovery.py

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
-def main():
 
+
+def main():
     for p in dir(hidden_4):
         if p[0] != '_' and p[1] != '_':
             print("{}".format(p))
+
 
 if __name__ == "__main__":
     import hidden_4
