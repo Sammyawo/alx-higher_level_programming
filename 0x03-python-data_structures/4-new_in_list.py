fde7e7a5beb4e04aeb1c8e993c11f52a1275b599
Samuel <awosamuel@gmail.com>
2022-07-02 04:43:02 -0700

    new_in_list.py

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-def new_in_list(my_list, idx, element): 
+def new_in_list(my_list, idx, element):
     lists = my_list[:]
     if idx >= 0 and idx <= len(lists) - 1:
         lists[idx] = element
