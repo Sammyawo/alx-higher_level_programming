b95df9d113265c598131049468f63d0511c4cc21
Samuel <awosamuel@gmail.com>
2022-07-19 05:48:19 -0700

    square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Square with size"""
+
+
+class Square:
+    """ initializing an instance called size
+
+    in the Square class
+
+    """
+    def __init__(self, size):
+        """ Private instance attribute
+
+        __size
+
+        """
+        self.__size = size
