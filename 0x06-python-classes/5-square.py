c05b13b2417e567b470a8cbabea0ab7793d0deee
Samuel <awosamuel@gmail.com>
2022-07-20 05:10:23 -0700

    square.py

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -14,9 +14,9 @@ class Square:
     def __init__(self, size=0):
         """ Initializing the attribute """
         self.__size = size
-        if not isinstance(size, int):
+        if not isinstance(self.__size, int):
             raise TypeError("size must be an integer")
-        elif value < 0:
+        elif self.__size < 0:
             raise ValueError("size must be >= 0")
 
     def area(self):
