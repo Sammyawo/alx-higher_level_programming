560715bfdfbed0e3491e7fd4acf920641e14346b
Samuel <awosamuel@gmail.com>
2022-07-25 04:27:12 -0700

    rectangle.py

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""0-rectangle module
+
+Empty class Rectangle that defines a rectangle
+
+"""
+
+class Rectangle:
+    """Empty class Rectangle that defines a rectangle"""
+    pass
