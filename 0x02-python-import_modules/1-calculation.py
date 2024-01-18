93ca7aa8bbdb2e929cad08ed5b22bbe957ea35bb
Samuel <awosamuel1@gmail.com>
2024-01-18 21:12:41 +0100

    .updated clear

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+from calculator_1 import *
+
+
+def main():
+    a = 10
+    b = 5
+    print(f'{a:d} + {b:d} = {add(a, b):d}')
+    print(f'{a:d} - {b:d} = {sub(a, b):d}')
+    print(f'{a:d} * {b:d} = {mul(a, b):d}')
+    print(f'{a:d} / {b:d} = {div(a, b):d}')
+
+if __name__ == "__main__":
+    main()
