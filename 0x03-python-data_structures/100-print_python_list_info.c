fb8cd2bf99fe5565a548dad07b719916f2ba51c6
Samuel <awosamuel1@gmail.com>
2024-01-22 21:12:59 +0100

    updated

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -1,23 +1,20 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include <Python.h>
 /**
-  * print_python_list_info - Print python list info
-  * @p: modulo object
-  * Return: Nothing
-  */
+ * print_python_list_info - Prints basic info about Python lists.
+ * @p: A PyObject list.
+ */
 void print_python_list_info(PyObject *p)
 {
-	Py_ssize_t i, size = (((PyVarObject *)(p))->ob_size);
-	Py_ssize_t allo = (((PyListObject *)(p))->allocated);
-	PyObject *ele;
-
-	printf("[*] Size of the Python List = %zd\n", size);
-	printf("[*] Allocated = %zd\n", allo);
-
-	for (i = 0; i < size; i++)
-	{
-		ele = PyList_GetItem(p, i);
-		printf("Element %zd: %s\n", i, (char *)(ele->ob_type)->tp_name);
-	}
+        int size, alloc, i;
+        PyObject *obj;
+        size = Py_SIZE(p);
+        alloc = ((PyListObject *)p)->allocated;
+        printf("[*] Size of the Python List = %d\n", size);
+        printf("[*] Allocated = %d\n", alloc);
+        for (i = 0; i < size; i++)
+        {
+                printf("Element %d: ", i);
+                obj = PyList_GetItem(p, i);
+                printf("%s\n", Py_TYPE(obj)->tp_name);
+        }
 }
