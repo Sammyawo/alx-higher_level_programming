e2412a391ed0b9ae5d3ad4a35d34d7a4a51ea11c
Samuel <awosamuel1@gmail.com>
2024-02-19 15:30:53 +0100

    updated

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -7,6 +7,6 @@
 
 def add_attribute(cla, attri, value):
     """Function that adds a new attribute to an object if it's possible"""
-    if not(hasattr(cla, '__dict__')):
+    if not (hasattr(cla, '__dict__')):
         raise TypeError("can't add new attribute")
     setattr(cla, attri, value)
